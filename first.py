4d5222c06ba0801e06b2a661579fb33e955b8538
Allan Engelhardt <Allan.Engelhardt@barnett-waddingham.co.uk>
2025-09-18 11:04:10 +0000

    First commit

---
+++ first.py
@@ -0,0 +1,17 @@
+## This is my first python program
+# %%
+print("Reserving is fun")
+print("Python is fun")
+# %%
+print("Reserving with python is almost too much fun!")
+
+
+# %%
+x = 6 * 7
+
+print(x)
+
+# %%
+
+print(f"The answer is {x}.")
+# %%
